8531f63a646e3e869b9fdf452226df1d35d6eeae
56613389+fabiopapaiss@users.noreply.github.com <fabiopapais0@gmail.com>
2020-05-29 11:30:58 -0300

    Minor inprovements; Started addCard

--- script.js
+++ script.js
@@ -1,50 +1,75 @@
 const cards = document.querySelectorAll('.card')
 const dropzones = document.querySelectorAll('.dropzone')
+const addCardsButtons = document.querySelectorAll('.material-icons')
 
+/** cards */
 cards.forEach(card => {
     card.addEventListener('dragstart', dragstart)
-    card.addEventListener('drag', drag)
+    // card.addEventListener('drag', drag)
     card.addEventListener('dragend', dragend)
 })
 
-function dragstart(event) {
-    dropzones.forEach(dropzone => dropzone.classList.add('highlight'))
-    event.target.classList.add('is-dragging') // event.target é o card
-}
-function drag() {
-    // console.log("dragging")
-}
-function dragend(event) {
-    // console.log("end dragging")
-    dropzones.forEach(dropzone => dropzone.classList.remove('highlight'))
-    event.target.classList.remove('is-dragging') // event.target é o card
+function dragstart() {
+    // console.log('CARD: Start dragging ')
+    dropzones.forEach( dropzone => dropzone.classList.add('highlight'))
+
+    // this = card
+    this.classList.add('is-dragging')
 }
 
-dropzones.forEach(dropzone => {
-    dropzone.addEventListener('dragenter', dragenter)
+// function drag() {
+//     console.log('CARD: Is dragging ')
+// }
+
+function dragend() {
+    // console.log('CARD: Stop drag! ')
+    dropzones.forEach( dropzone => dropzone.classList.remove('highlight'))
+
+    // this = card
+    this.classList.remove('is-dragging')
+}
+
+/** dropzones recognition and appends */
+dropzones.forEach( dropzone => {
+    // dropzone.addEventListener('dragenter', dragenter)
     dropzone.addEventListener('dragover', dragover)
     dropzone.addEventListener('dragleave', dragleave)
-    dropzone.addEventListener('drop', ondrop)
+    dropzone.addEventListener('drop', drop)
 })
 
-function dragenter() {
-    // console.log("dragenter")
-}
-function dragover(event) {
-    //event é o dropzone
-    // console.log("dragover")
-    event.target.classList.add('is-over')
+// function dragenter() {
+//     console.log('DROPZONE: Enter in zone ')
+// }
 
-    // dragging card
-    const cardBeingGrabbed = document.querySelector('.is-dragging')
+function dragover() {
+    // this = dropzone
+    this.classList.add('is-over')
 
-    event.target.appendChild(cardBeingGrabbed)
+    // get dragging card
+    const cardBeingDragged = document.querySelector('.is-dragging')
+
+    // this = dropzone
+    this.appendChild(cardBeingDragged)
 }
-function dragleave(event) {
-    console.log("drageLeave")
-    event.target.classList.remove('is-over')
+
+function dragleave() {
+    // console.log('DROPZONE: Leave ')
+    // this = dropzone
+    this.classList.remove('is-over')
 }
-function ondrop(event) {
-    console.log("aqui")
-    event.target.classList.remove('is-over')
+
+function drop() {
+    // console.log('DROPZONE: dropped ')
+    this.classList.remove('is-over')
+}
+
+
+/* addCard functions */
+addCardsButtons.forEach( addCardsButton => {
+    addCardsButton.addEventListener('click', addCard)
+})
+
+function addCard() {
+    let task = prompt('What is the name of the task?', 'Start this project on Github')
+    
 }
