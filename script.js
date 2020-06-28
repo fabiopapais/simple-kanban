10a1ccdbf32bb42b58e23dbf0c2749d8aacc854f
56613389+fabiopapaiss@users.noreply.github.com <fabiopapais0@gmail.com>
2020-06-28 16:38:42 -0300

    feat: Dynamically cards creation

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const newTasks = JSON.parse(localStorage.getItem('newTasks')) || [] // TODO1: localStorage implement
+const newTasks = JSON.parse(localStorage.getItem('newTasks')) || []
 
 var cards = document.querySelectorAll('.card')
 var dropzones = document.querySelectorAll('.dropzone')
@@ -9,7 +9,7 @@ const doneDiv = document.getElementById('doneDiv')
 
 let colors = ['red', 'green', 'blue']
 
-function load() {
+function load(first) {
     cards = document.querySelectorAll('.card')
     dropzones = document.querySelectorAll('.dropzone')
 
@@ -21,16 +21,21 @@ function load() {
     })
 
     /** dropzones recognition and appends */
-    dropzones.forEach( dropzone => {
+    dropzones.forEach(dropzone => {
         // dropzone.addEventListener('dragenter', dragenter)
         dropzone.addEventListener('dragover', dragover)
         dropzone.addEventListener('dragleave', dragleave)
         dropzone.addEventListener('drop', drop)
     })
 
-
+    if (first) {
+        // localStorage cards render
+        newTasks.forEach(task => {
+        createCard(task.type, task.name)
+        })
+    }
 }
-load()
+load(true)
 
 function dragstart() {
     // console.log('CARD: Start dragging ')
@@ -53,6 +58,8 @@ function dragover() {
     // get dragging card
     const cardBeingDragged = document.querySelector('.is-dragging')
 
+    console.log(cardBeingDragged)
+
     // this = dropzone
     this.appendChild(cardBeingDragged)
 }
@@ -66,31 +73,59 @@ function drop() {
     this.classList.remove('is-over')
 }
 
-function createCard(type) {
-    const task = prompt('Please write your task below:') || "Star this project on Github" // oh! looks like an easter egg!
+function handleCreateCard(type) {
+    const taskName = prompt('Please write your task below:') || "Star this project on Github" // oh! looks like an easter egg!
 
-    newTasks.push(task) // TODO1
-    localStorage.setItem('newTasks', JSON.stringify(newTasks)) // TODO1
+    const task = {
+        name: taskName,
+        type: type
+    }
 
+    newTasks.push(task) 
+    localStorage.setItem('newTasks', JSON.stringify(newTasks))
+
+    createCard(type, taskName)
+}
+
+function createCard(type, task) {
     let randcolor = colors[Math.floor(Math.random() * (3 - 0) ) + 0]
 
-    var newCard = [
-        '<div class="card" draggable="true">',
-            `<div class="status ${randcolor}">` + '</div>',
-            '<div class="content">' + task + '</div>',
-        '</div>'
-    ].join("\n");
+    // TODO  test another simple ways to render html
+    // let parser = new DOMParser()
+    // let el = parser.parseFromString(
+    //     `<div class="card" draggable="true">
+    //         <div class="status ${randcolor}"></div>
+    //         <div class="content">${task}</div>
+    //     </div>`
+    // , "text/html")
+
+    let card = document.createElement('div')
+    card.classList.add('card')
+    card.setAttribute('draggable', 'true')
+    
+    let status = document.createElement('div')
+    status.classList.add('status')
+    status.classList.add(randcolor)
+
+    let content = document.createElement('div')
+    content.classList.add('content')
+    content.innerHTML = task
+
+    card.appendChild(status)
+    card.appendChild(content)
+
+    console.log(card)
 
     switch (type) {
         case "todo":
-            todoDiv.innerHTML += newCard
+            todoDiv.appendChild(card)
             break
         case "progress":
-            progressDiv.innerHTML += newCard
+            progressDiv.appendChild(card)
             break
         case "done":
-            doneDiv.innerHTML += newCard
+            doneDiv.appendChild(card)
             break
     }
-    load()
+    load(false)
 }
